4f00eaa9874a4195ec896368f99fa71e75dcdf83
Laetitia Taddei <laetdi@gmail.com>
2023-01-02 09:20:21 +0100

    study pokedex data and correct missing values

--- main.py
+++ main.py
@@ -1,3 +1,29 @@
 import pandas as pd
 import os
 
+filesList = os.listdir('./data')
+for file in filesList:
+    print(file)
+
+
+pokemons = pd.read_csv('data/pokedex.csv', encoding='latin_1')
+# check the df
+print(pokemons.head())
+# check the columns
+print(pokemons.columns.values)
+# change LEGENDAIRE feature values to digit
+pokemons['LEGENDAIRE'] = (pokemons['LEGENDAIRE'] == 'VRAI').astype(int)
+print(pokemons['LEGENDAIRE'])
+
+# check if missing datas
+print(pokemons.shape)
+print(pokemons.info())
+
+# find the missing name
+print(pokemons[pokemons['NOM'].isnull()])
+# find pokemons around him and look in online pokedex
+print(pokemons['NOM'][61])
+print(pokemons['NOM'][63])
+# add the name
+# pokemons['NOM'][62] = 'Colossinge'
+print(pokemons['NOM'][62])
