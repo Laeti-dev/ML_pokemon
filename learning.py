9fdd8b6b2eda8a6c877e5ee34e78b59ba1b5d8db
Laetitia Taddei <laetdi@gmail.com>
2023-01-06 10:33:40 +0100

    install scikit-learn - split data into training and test - multiple linear regression precision

--- learning.py
+++ learning.py
@@ -1,5 +1,7 @@
 import pandas as pd
 
+
+# STEP 1 = cut observations into learning sets (X) and test sets
 dataset = pd.read_csv('./data/dataset.csv', delimiter='\t')
 
 print(dataset.shape)
@@ -8,3 +10,37 @@ print(dataset.info())
 # delete observations without values
 dataset = dataset.dropna(axis=0, how='any')
 print(dataset.shape)
+print(dataset)
+
+# learning set X
+X = dataset.iloc[:, 5:11].values
+print(X)
+
+# features to predict == victory_ratio y
+y = dataset.iloc[:, 16].values
+print(y)
+
+# create learning and test sets
+from sklearn.model_selection import train_test_split
+X_LEARNING, X_VALIDATION, y_LEARNING, y_VALIDATION = train_test_split(X,y, test_size=0.2, random_state=0)
+# test_size = 0.2 is to set 20% of our observation for tests
+# since we have many predictive variables for one prediction => multiple linear regression
+
+# STEP 2 : learning
+from sklearn.metrics import r2_score
+from sklearn.linear_model import LinearRegression
+
+# algorithm choice:
+algorithm = LinearRegression()
+
+# Learning thanks to fit function on learning set
+algorithm.fit(X_LEARNING, y_LEARNING)
+
+# Prediction on test set (VALIDATION)
+predictions = algorithm.predict(X_VALIDATION)
+
+# Evaluate learning precision with r2_score function by conparing predicted (predictions)
+# and expected values (y_VALIDATION)
+precision = r2_score(y_VALIDATION, predictions)
+
+print('Precision =' +str(precision))
